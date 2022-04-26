71925deb4791c1e7f55bfa522ddf905c3bc52c48
hwhong <zusdn90@naver.com>
2022-04-26 17:41:23 +0900

    mock API 생성

--- backend/app/api/v1/users.py
+++ backend/app/api/v1/users.py
@@ -1,6 +1,6 @@
 import jwt
 from datetime import datetime, timedelta
-from fastapi import APIRouter, Depends,status
+from fastapi import APIRouter, Depends, status
 
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
@@ -19,26 +19,87 @@ LOGGER = base_logger()
 user_router = APIRouter(route_class=ExceptionRoute)
 
 
-@user_router.post("/", status_code=201, response_model=Token)
+@user_router.post(
+    "/",
+    status_code=201,
+    response_model=Token,
+    summary="회원가입",
+)
 async def register(user: UserCreate, db: Session = Depends(get_db)) -> JSONResponse:
     """
-    `회원가입 API`\n
-    :return:
+    `회원가입 API`
     """
     db_user = crud.get_user_by_phone_number(db, phone_number=user.phone_number)
-    
+
     if not user.phone_number or not user.password:
         raise CommonException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Phone Number and PW must be provided."
-            )
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Phone Number and PW must be provided.",
+        )
     if db_user:
         raise CommonException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Phone Number already registered."
-            )
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Phone Number already registered.",
+        )
     new_user = crud.create_user(db=db, user=user)
-    token = dict(Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(new_user).dict(),)}")
-    
+    token = dict(
+        Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(new_user).dict(),)}"
+    )
+
     return token
+
+
+@user_router.post(
+    "/auth",
+    status_code=200,
+    summary="전화번호 인증",
+)
+async def auth(user: UserCreate, db: Session = Depends(get_db)) -> JSONResponse:
+    """
+    `전화번호 인증 API`
+    """
+
+    return ""
+
+
+@user_router.post(
+    "/login",
+    status_code=200,
+    summary="로그인",
+)
+async def login(user: UserCreate, db: Session = Depends(get_db)) -> JSONResponse:
+    """
+    `로그인 API`
+    """
+
+    return ""
+
+
+@user_router.get(
+    "/",
+    status_code=200,
+    summary="회원정보 조회",
+)
+async def users_info(db: Session = Depends(get_db)) -> JSONResponse:
+    """
+    `회원정보 조회 API`
+    """
+
+    return ""
+
+
+@user_router.post(
+    "/pwd/reset",
+    status_code=200,
+    summary="비밀번호 찾기",
+)
+async def reset_password(db: Session = Depends(get_db)) -> JSONResponse:
+    """
+    `비밀번호 찾기 API`\n
+    `전화번호 인증 후 비밀번호 초기화`
+    """
+
+    return ""
 
 
 def create_access_token(*, data: dict = None, expires_delta: int = None):
